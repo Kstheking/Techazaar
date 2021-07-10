f06f46b166bcfe41f99f48beea9e5a47ca6bb12d
Khushal Sahni <kstheking0@gmail.com>
2021-07-10 21:46:48 +0530

    change the changeDetection strategy

--- src/app/product/product/product.component.ts
+++ src/app/product/product/product.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
   @Input() public product!: Product;
