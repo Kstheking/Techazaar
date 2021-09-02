704e6b8ff4dea26d3fca910f7d500c62382fd282
Khushal Sahni <kstheking0@gmail.com>
2021-09-02 10:57:58 +0530

    nuke

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,2 @@
+//we can write a test in here for checking that the product list indeed gets refreshed when that event is emitted by
+//create product component
--- src/app/product/create-product-reactive/create-product-reactive.component.spec.ts
+++ src/app/product/create-product-reactive/create-product-reactive.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { ProductService } from 'src/app/services/product.service';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,6 +8,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { inject } from '@angular/core/testing';
 import { CreateProductReactiveComponent } from './create-product-reactive.component';
+import { Product } from 'src/app/models/product';
 
 
 describe('CreateProductReactiveComponent', () => {
@@ -34,7 +35,74 @@ describe('CreateProductReactiveComponent', () => {
     httpBackend = backend;
     fixture = TestBed.createComponent(CreateProductReactiveComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  }));
+
+  it('should not do anything when form data is invalid', fakeAsync(() => {
+    component.resetForm();
+    component.submitForm();
+    httpBackend.expectNone('/api/product', 'add product api call');
+  }));
+
+  it('should create product on correct data', fakeAsync(() => {
+    component.productForm.setValue({
+      name: 'Test product',
+      price: 20,
+      url: 'http://test.product.com',
+      onSale: false
+    });
+    component.submitForm();
+    const httpReq = httpBackend.expectOne('/api/product', 'create product call');
+    expect(httpReq.request.method).toEqual('POST');
+    const product = new Product(null, 'http://test.product.com', 'Test product', 20, 0, 10 );
+    expect(httpReq.request.body).toEqual(product);
+
+    spyOn(component.refreshProducts, 'emit'); //first make jasmine spy on the eventEmitter's emit method
+
+    httpReq.flush({
+      msg: "Test message"
+    }, {
+      status: 200
+    });
+    tick();
+    expect(component.refreshProducts.emit).toHaveBeenCalledWith("refreshItBrother");
+    
+    expect(component.productForm.getRawValue).toEqual({
+      name: null,
+      price: 0,
+      url: null,
+      onSale: false
+    });
+  }));
+
+  it('should not create product on incorrect data', fakeAsync(() => {
+    component.productForm.setValue({
+      name: 'Test product',
+      price: 20,
+      url: 'http://test.product.com',
+      onSale: false
+    });
+    component.submitForm();
+    const httpReq = httpBackend.expectOne('/api/product', 'create product call');
+    expect(httpReq.request.method).toEqual('POST');
+    const product = new Product(null, 'http://test.product.com', 'Test product', 20, 0, 10 );
+    expect(httpReq.request.body).toEqual(product);
+
+    spyOn(component.refreshProducts, 'emit'); //first make jasmine spy on the eventEmitter's emit method
+
+    httpReq.flush({
+      msg: "Product seems to already have an id assigned"
+    }, {
+      status: 400
+    });
+    tick();
+    expect(component.refreshProducts.emit).not.toHaveBeenCalled();
+    
+    expect(component.productForm.getRawValue).toEqual({
+      name: 'Test product',
+      price: 20,
+      url: 'http://test.product.com',
+      onSale: false
+    });
   }));
 
 });
